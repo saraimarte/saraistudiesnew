5dd5f5139ca72d460f981ca06c2f4a51adc33fe8
Sarai Marte <smartepujols@gmail.com>
2024-08-25 00:51:57 -0400

    082524 got calc1 slug and index to work

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+const calc1Collection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updatedDate: z.date(),
+    draft: z.boolean(),
+    author: z.string(),
+  }),
+});
+
+
+export const collections = {
+  calc1: calc1Collection
+};
