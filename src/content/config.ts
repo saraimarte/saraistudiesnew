b36b5d24349134a4faf9ff8e09c5506083d67146
Sarai Marte <smartepujols@gmail.com>
2024-08-29 02:29:41 -0400

    082824 increased font size for posts from 0.9 to 1rem + added calc 2 class and some posts + added Solution, Problem and ImageMd component+ added gray dashed bottom border to the postInfo as a separation

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,19 @@ const calc1Collection = defineCollection({
   }),
 });
 
+const calc2Collection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updatedDate:z.date(),
+    draft: z.boolean(),
+    author: z.string(),
+  }),
+});
+
 
 export const collections = {
-  calc1: calc1Collection
+  calc1: calc1Collection,
+  calc2: calc2Collection
 };
