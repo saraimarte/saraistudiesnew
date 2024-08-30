446db4c699d6d88fa5df90c5912340cafad04232
Sarai Marte <smartepujols@gmail.com>
2024-08-29 21:22:40 -0400

    082924 added supply chain and writing for media class

--- src/content/config.ts
+++ src/content/config.ts
@@ -45,9 +45,33 @@ const statMethodsCollection = defineCollection({
 });
 
 
+const supplyChainCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updatedDate:z.date(),
+    draft: z.boolean(),
+    author: z.string(),
+  }),
+});
+
+const writingForMediaCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updatedDate:z.date(),
+    draft: z.boolean(),
+    author: z.string(),
+  }),
+});
+
 export const collections = {
   calc1: calc1Collection,
   calc2: calc2Collection,
   macc: maccCollection,
-  statMethods: statMethodsCollection
+  statMethods: statMethodsCollection,
+  supplyChain:supplyChainCollection,
+  writingForMedia: writingForMediaCollection
 };
