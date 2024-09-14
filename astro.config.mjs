b99b3beff30863fb54d4b8813c855c0955899703
Sarai Marte <smartepujols@gmail.com>
2024-09-14 01:28:45 -0400

    081324 updated pageFind?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import PagefindHighlight from 'public/pagefind/pagefind-highlight';
+import pagefind from 'astro-pagefind';
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(),pagefind()],
   markdown: {
    syntaxHighlight: "prism",
   },
