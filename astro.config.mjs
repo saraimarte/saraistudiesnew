9bdc104ec8b1a039d0e56b39b36d4d3ed1c33f54
Sarai Marte <smartepujols@gmail.com>
2024-09-14 01:30:25 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import PagefindHighlight from 'public/pagefind/pagefind-highlight';
 import pagefind from 'astro-pagefind';
 
 export default defineConfig({
