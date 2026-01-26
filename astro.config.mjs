a47bb4794ccb0b3268c1fd669fcbc356978fa0f6
Sarai Marte <smartepujols@gmail.com>
2026-01-26 04:15:15 -0500

    added assets alias in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
+import path from 'path';
 
 export default defineConfig({
-  integrations: [mdx(),pagefind()],
+  integrations: [mdx(), pagefind()],
   markdown: {
-   syntaxHighlight: "prism",
+    syntaxHighlight: "prism",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve('./src/assets'),
+      },
+    },
   },
 });
