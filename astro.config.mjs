7d5f54b00a077eb39c56ca70c03ef032b40034f7
Sarai Marte <smartepujols@gmail.com>
2026-01-26 04:18:54 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
-import path from 'path';
 
 export default defineConfig({
-  integrations: [mdx(), pagefind()],
+  integrations: [mdx(),pagefind()],
   markdown: {
-    syntaxHighlight: "prism",
-  },
-  vite: {
-    resolve: {
-      alias: {
-        '@assets': path.resolve('./src/assets'),
-      },
-    },
+   syntaxHighlight: "prism",
   },
 });
