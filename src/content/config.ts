403e301723c449743b32b6f36d4357afddcbbf12
Sarai Marte <smartepujols@gmail.com>
2026-06-05 22:19:29 -0400

    added internet tech + discrete 2 set operations w demorgans law video

--- src/content/config.ts
+++ src/content/config.ts
@@ -332,6 +332,18 @@ const businessEthicsCollection = defineCollection({
 });
 
 
+const internetTechnologyCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updatedDate:z.date(),
+    draft: z.boolean(),
+    author: z.string()
+  }),
+});
+
+
 export const collections = {
   calc1: calc1Collection,
   calc2: calc2Collection,
@@ -361,5 +373,6 @@ export const collections = {
   advancedCorporateFinance: advancedCorpFinanceCollection,
   internationalFinancialManagement: internationalFinancialManagementCollection,
   dataScience: dataScienceCollection, 
-  businessEthics: businessEthicsCollection
+  businessEthics: businessEthicsCollection, 
+  internetTechnology:internetTechnologyCollection
 };
